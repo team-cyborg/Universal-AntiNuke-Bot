1d3e2d94dbbc809e90403435f1c5304298363dde
Dev Insane <110784602+bugsum@users.noreply.github.com>
2022-09-20 03:44:04 +0000

    increased msg delete delay, fixed minor interaction crash

--- events/msgCreate.js
+++ events/msgCreate.js
@@ -1,35 +1,35 @@
 const { Client, Message } = require("discord.js");
 
 module.exports = {
-      name: 'messageCreate',
-      once: false,
+  name: 'messageCreate',
+  once: false,
 
-      /**
-       * 
-       * @param {Client} client 
-       * @param {Message} message 
-       */
-      run: async (client, message) => {
-            if (message.mentions.users.has(client.user.id)) {
-                  message.reply({
-                        embeds: [
-                              {
-                                    title: 'Universal™',
-                                    description: [
-                                          '> Greetings, I am universal, a discord antinuke bot dedicated to preventing guild nukes.',
-                                          '> Use ` /help ` to get started!'
-                                    ].join('\n'),
-                                    color: 0x5440cd,
-                                    thumbnail: {
-                                          url: client.user.displayAvatarURL()
-                                    }
-                              }
-                        ]
-                  }).then((msg) => {
-                        setTimeout(() => {
-                              if (msg.deletable) msg.delete(); else return
-                        }, 3000);
-                  })
+  /**
+   * 
+   * @param {Client} client 
+   * @param {Message} message 
+   */
+  run: async (client, message) => {
+    if (message.mentions.users.has(client.user.id)) {
+      message.reply({
+        embeds: [
+          {
+            title: 'Universal™',
+            description: [
+              '> Greetings, I am universal, a discord antinuke bot dedicated to preventing guild nukes.',
+              '> Use ` /help ` to get started!'
+            ].join('\n'),
+            color: 0x5440cd,
+            thumbnail: {
+              url: client.user.displayAvatarURL()
             }
-      }
+          }
+        ]
+      }).then((msg) => {
+        setTimeout(() => {
+          if (msg.deletable) msg.delete(); else return
+        }, 10000);
+      })
+    }
+  }
 }
